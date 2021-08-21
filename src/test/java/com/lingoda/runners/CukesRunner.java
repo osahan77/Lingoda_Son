5cdf058fea2a58455f28c9b5170bf2d51cf28852
OnderSahan <osahan77@gmail.com>
2021-08-21 14:28:32 +0300

    initial commit

--- src/test/java/com/lingoda/runners/CukesRunner.java
+++ src/test/java/com/lingoda/runners/CukesRunner.java
@@ -16,13 +16,5 @@ import org.junit.runner.RunWith;
 
  )
 public class CukesRunner {
- /*
- Write a basic browser automation framework to validate a Polymer website. The focus should be on the interaction with the browser.
-The Web Application under test http://todomvc.com/
-The first step should be to load the website, click within the JavaScript tab, and select the Polymer link.
-The second step should be: Add two Todo items
-Bonus: (optional stretch goal): Edit the content of the second Todo item
- If there are other major priorities or urgent matters that prevent you from completing the challenge, that is understandable, just let me know when you can complete it.
-Please confirm you received the test.
-  */
+
 }
